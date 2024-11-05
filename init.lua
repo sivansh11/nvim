c9f3a916cb47ad643b9131da85e7f2607c208d4d
sivansh11 <sivanshgupta2002@gmail.com>
2024-11-05 09:55:13 +0530

    changed tab to 2

--- init.lua
+++ init.lua
@@ -111,8 +111,8 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- shiftwidth
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 -- vim.opt.expandtab = true
 -- vim.bo.softtabstop = 2
 -- vim.opt.smarttab = true
