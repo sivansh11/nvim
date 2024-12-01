698689785395952aa38197b166f2340d07e60e0f
sivansh11 <sivanshgupta2002@gmail.com>
2024-12-02 00:40:10 +0530

    added startup plugin

--- init.lua
+++ init.lua
@@ -1015,6 +1015,13 @@ require('lazy').setup({
     },
   },
   {
+    'startup-nvim/startup.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+    config = function()
+      require('startup').setup()
+    end,
+  },
+  {
     'nvim-neo-tree/neo-tree.nvim',
     config = function()
       vim.keymap.set('n', '<leader>nt', '<cmd>Neotree<cr>', { desc = 'Neotree' })
